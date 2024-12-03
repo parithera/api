d66e99d424f3221d58545757d5f73e726de9b3ef
CedricHerzog <CedricHerzog@users.noreply.github.com>
2024-12-03 11:50:28 +0100

    Remove enterprise modules

--- src/enterprise_modules/enterprise.module.ts
+++ src/enterprise_modules/enterprise.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ChatModule } from './chat/chat.module';
-import { GraphModule } from './graphs/graphs.module';
 
 @Module({
-    imports: [ChatModule, GraphModule],
+    imports: [],
     providers: [],
     controllers: []
 })
